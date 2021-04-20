4e4469667eedb5e6fde43cf7e2e856638799a9eb
PKizumiBeta <pkizumi.beta@gmail.com>
2021-04-20 16:19:10 +0900

    2021/04/21-008

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/git-nuxt-project/favicon.ico' }
     ]
   },
 
